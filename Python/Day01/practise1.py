bb886d705931594841e2ca081193d2fb03881e22
Pratik Patil <pratikpatil7612@gmail.com>
2026-07-05 15:15:39 +0530

    Day1: Started my Developer Journey

---
+++ Python/Day01/practise1.py
@@ -0,0 +1,9 @@
+name = "Pratik"
+age = 20
+city = "Mumbai"
+game = "Chess"
+
+print("My name is", name)
+print("I am", age, "years old.")
+print("I live in", city)
+print("My favorite game is", game)
---
+++ Python/Day01/program1.py
@@ -0,0 +1,6 @@
+print("My name is Pratik.")
+print("Today is my first day learning Python.")
+print("I will become a Full Stack Developer.")
+
+name = "Pratik"
+print(name)
