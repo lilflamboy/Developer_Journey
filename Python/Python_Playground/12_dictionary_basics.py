daf6d746cd25ecba1532e60d4bb90264fe4a8d65
Pratik Patil <pratikpatil7612@gmail.com>
2026-08-21 20:52:49 +0530

    Add dictionary basic and dictionary practice

---
+++ Python/Python_Playground/12_dictionary_basics.py
@@ -0,0 +1,7 @@
+student = {}
+student["name"]= input("enter your name: ")
+student["age"]= int(input("enter your age: "))
+student["marks"]= int(input("enter your marks: "))
+student["city"]= input("enter your city: ")
+print(student)
+
