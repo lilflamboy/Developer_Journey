3fc640490a600d23aae14828af34a0786e58abe1
Pratik Patil <pratikpatil7612@gmail.com>
2026-08-25 20:29:51 +0530

    Practice dynamic student lists

--- Python/Python_Playground/13_list_dictionary_practice.py
+++ Python/Python_Playground/13_list_dictionary_practice.py
@@ -1,4 +1,4 @@
-students = [
+"""students = [
     {"name": "Pratik", "age": 20},
     {"name": "Rahul", "age": 22},
     {"name": "Amit", "age": 20}
@@ -8,4 +8,27 @@ print(students)
 students[2]["age"] = 21
 for student in students:
     if student["age"] > 20:
-        print(student["name"])
+        print(student["name"])"""
+
+"""students = []
+student = {}      
+input_name = input("name: ")
+student["name"] = input_name
+input_age = input("age: ")
+student["age"] = int(input_age)
+students.append(student)
+print(students)"""
+
+students = []
+for i in range(3):
+    student = {}
+    input_name = input("name: ")
+    student["name"] = input_name
+    input_age = input("age: ")
+    student["age"] = int(input_age)
+    students.append(student)
+print(students)    
+for student in students:
+    if student["age"] > 20:
+        print(student["name"] + " is older than rest of the students.")
+
